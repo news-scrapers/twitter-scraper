5e7c94fbd5141dd3937d210d7d1bb8388f0eb040
Hugo J. Bello <hugojose@azlo.com>
2019-04-29 19:36:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const Twitter = require('twitter');
+
+const client = new Twitter({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_TOKEN,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+// https://developer.twitter.com/docs/things-every-developer-should-know
+client.get('search/tweets', {q: 'suicidio'}, function(error, tweets, response) {
+    console.log(tweets);
+    tweets.statuses.forEach(function(tweet) {
+        console.log("tweet: -----" + tweet.text);
+        console.log(tweet.created_at);
+        //console.log(tweet);
+    });
+});
